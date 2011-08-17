989d2ae832ce31ccf8f4069029af54adae2204dc
Raphael Savina <raphael@savina.net>
2011-08-17 14:40:24 +0100

    New version of Admin page.
    It includes: -Insert image into database -Run Fetch Images -Display
    latest 25 images -Links to individual images (only last 5 of each cams):

--- src/main.py
+++ src/main.py
@@ -9,7 +9,7 @@ from google.appengine.api import users
 from google.appengine.ext import webapp, blobstore
 from google.appengine.ext.webapp import blobstore_handlers, template
 from google.appengine.ext.webapp.util import run_wsgi_app
-from model import WebcamImage
+from model import WebcamImage, Webcam
 
 class MainPage(webapp.RequestHandler):
 	def get(self):
@@ -24,21 +24,34 @@ class MainPage(webapp.RequestHandler):
 
 class AdminPage(webapp.RequestHandler):
 	def get(self):
-		im = WebcamImage.all()
-		im.order("webcam")
-		im.order("-timestamp")
-		# limiting pictures to 25 - otherwise we may run out of CPU quota
-		results = im.fetch(1000)
-		# rewrite the values - blob is a BlobReferenceProperty, but we need to pass
-		# the key
-		pic_blobs = [{"blob": i.blob.key(),
-	                  "webcam": i.webcam,
-	                  "size": i.blob.size,
-	                  "timestamp": i.timestamp}
-	                   for i in results]
+		cam = Webcam().all()
+		cam.order("name")
+		listcams = cam.fetch(300)	    
+		counti = 0
+		pic_blobs = []
+		for j in listcams:
+			try:
+				im = WebcamImage.all()
+				im.filter("webcam =", j.name.lower())
+				im.order("webcam")
+				im.order("-timestamp")
+				results = im.fetch(5)
+				for i in results:
+					try: 
+						pic_blobs = pic_blobs + [{"blob": i.blob.key(),"webcam": i.webcam,"size": i.blob.size,"timestamp": i.timestamp, "count" : counti}]
+						# logging.info("loop: %s/%s" % (countj,counti))
+						counti = counti + 1
+					except Exception, e:
+						logging.error("Error fetching data: %s" % e)
+				counti = 0
+			except Exception, f:
+				logging.error("Error fetching data: %s" % f)
+		cam_name = [{"name":j.name}for j in listcams]
+
 		template_values = {
-			"images": pic_blobs,
+			"images": pic_blobs, "names" : cam_name
 	    }
+
 		path = os.path.join(os.path.dirname(__file__), "admin.html")
 		self.response.out.write(template.render(path, template_values))
 
